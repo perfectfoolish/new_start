59c311e2b2642bcdfc40319b66f4156b3a6b47ec
perfectfoolish <DZKXperfect@gmail.com>
2015-10-31 10:34:05 +0800

    iii

---
+++ components/Action.js
@@ -0,0 +1,11 @@
+var Action = React.createClass({
+  render: function() {
+    return (
+
+      <div className="action clearfix">
+      <a href="#" className="action-button button" />
+      </div>
+      );
+  }
+});
+ReactDOM.render(<Action />, document.getElementById('action'));
---
+++ components/Books.js
@@ -0,0 +1,13 @@
+var Books = React.createClass({
+  render: function() {
+    return (
+
+      <div className="books clearfix">
+        <div className="book"><img src="images/github.jpg" alt className="cover" /></div>
+        <div className="book"><img src="images/tealeaf-http.jpg" alt className="cover" /></div>
+        <div className="book"><img src="images/tlcl-book.jpg" alt className="cover" /></div>
+      </div>
+    );
+  }
+});
+ReactDOM.render(<Books />, document.getElementById('books'));
